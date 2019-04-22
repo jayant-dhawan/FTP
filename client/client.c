8c32b82ab1de7d9c36a648179713a92e7ec19c27
jayant-dhawan <jayantdhawan95@gmail.com>
2019-04-23 00:45:17 +0530

    first commint

---
+++ client/client.c
@@ -0,0 +1,346 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/stat.h>
+#include <sys/sendfile.h>
+#include <fcntl.h>
+#include <ctype.h>
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+void errormsg(int error)
+{
+    if (error == 530)
+        printf("530 : You are not logged in\n ");
+    else if (error == 331)
+        printf("331 : Username exists password needed\n ");
+    else if (error == 332)
+        printf("332 : NO USER FOUND\n ");
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        fprintf(stderr, "Usage: client <ip address> <port number>");
+    }
+
+    int sockfd, port_no, i, n, size, status = 0, filehandle;
+    struct sockaddr_in serv_addr;
+    char buffer[100], command[5], filename[20], input[50], *f;
+    struct hostent *server;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error("Error in creating host");
+    }
+    port_no = atoi(argv[2]);
+
+    server = gethostbyname(argv[1]);
+    if (server == NULL)
+    {
+        fprintf(stderr, "No such host");
+    }
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr_list[0], (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(port_no);
+
+    //serv_addr.sin_addr.s_addr = inet_addr(inet_ntoa(*((struct in_addr *)server->h_addr_list[0])));
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("connection failed!");
+    i = 1;
+    printf("1. CRET: Create a new user (Usage: CRET <username> <password>)\n2. USER: Enter the username for login (Usage: USER <username>)\n3. PASS: Enter the password for login (Usage: PASS <password>)\n4. PWD: Print the current directory of server (Usage: PWD)\n5. CWD: Change the current directory of server (Usage: CWD <directory name>)\n6. MKD: Make a new directory on server (Usage: MKD <directory name>)\n7. RMD: Remove directory from server (Usage: RMD <direcctory name>)\n8. LIST: List all the files in current directory of server (Usage: LIST)\n9. STOR: Send the file to server (Usage: STOR <filename>)\n10. RETR: Retrieve a file from server (Usage: RETR <filename>)\n\n\n");
+
+    while (i)
+    {
+
+        n = read(sockfd, buffer, 100);
+        if (n == 0)
+        {
+            printf("Error Reading from server");
+        }
+        printf("%s", buffer);
+        bzero(input, 50);
+        bzero(buffer, 100);
+        fgets(input, 50, stdin);
+        if (strncmp(input, "CRET", 4) == 0 || strncmp(input, "cret", 4) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, sizeof(buffer));
+            if (n < 0)
+                error("Error in writing in CRET\n");
+            n = read(sockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in reading command in CRET\n");
+            //printf("status: %d",status);
+            if (status == 1)
+                printf("User created successfully\n");
+            else
+                printf("Error creating new user!\n");
+        }
+
+        else if (strncmp(input, "USER", 4) == 0 || strncmp(command, "user", 4) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error checking username");
+            n = read(sockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error getting username");
+            if (status)
+                errormsg(status);
+            else
+                printf("Undefined Error\n");
+        }
+        else if (strncmp(input, "PASS", 4) == 0 || strncmp(command, "pass", 4) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in matching password");
+            n = read(sockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in getting password");
+            //if(status)
+            //    errormsg(status);
+            //else
+            //    printf("error\n");
+        }
+        else if (strncmp(input, "PWD", 3) == 0 || strncmp(command, "pwd", 3) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in PWD\n");
+            n = read(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in reading command\n");
+            if (&buffer == 530)
+                errormsg(530);
+            else
+            {
+                printf("The path of the remote directory is: %s\n", buffer);
+            }
+        }
+        else if (strncmp(input, "CWD", 3) == 0 || strncmp(command, "cwd", 3) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in CWD\n");
+            //printf("\ndata sent");
+            n = read(sockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in reading command in CWD\n");
+            if (status == 1)
+                printf("Remote directory successfully changed\n");
+            else
+                errormsg(status);
+        }
+        else if (strncmp(input, "RMD", 3) == 0 || strncmp(command, "rmd", 3) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in RMD\n");
+            n = read(sockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in reading command in RMD\n");
+            if (status == 1)
+                printf("Remote directory successfully Deleted\n");
+            else
+                errormsg(status);
+        }
+        else if (strncmp(input, "MKD", 3) == 0 || strncmp(command, "mkd", 3) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in MKD\n");
+            n = read(sockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in reading command in MKD\n");
+            if (status == 1)
+                printf("Remote directory successfully Created\n");
+            else
+                errormsg(status);
+        }
+        else if (strncmp(input, "LIST", 4) == 0 || strncmp(command, "list", 4) == 0)
+        {
+            strcpy(buffer, "LIST");
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in LIST\n");
+            n = read(sockfd, &size, sizeof(int));
+            if (n < 0)
+                error("Error in reading command in LIST\n");
+            if (size == 530)
+            {
+                errormsg(size);
+            }
+            else
+            {
+                f = malloc(size);
+                read(sockfd, f, size);
+                filehandle = creat("list_file.txt", O_WRONLY);
+                n = write(filehandle, f, size);
+                if (n < 0)
+                    error("Error in writing in LIST\n");
+                close(filehandle);
+                printf("The remote directory listing is as follows:\n");
+                system("cat list_file.txt");
+            }
+        }
+        else if (strncmp(input, "STOR", 4) == 0 || strncmp(command, "stor", 4) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in STOR\n");
+            FILE *f;
+            int words = 0;
+            char c;
+            char file[50];
+            sscanf(buffer, " %*s %s", file);
+            f = fopen(file, "r");
+            while ((c = getc(f)) != EOF)
+            {
+                fscanf(f, " %s", buffer);
+                if (isspace(c) || c == '\t')
+                {
+                    words++;
+                }
+            }
+            n = write(sockfd, &words, sizeof(int));
+            if (n < 0)
+                error("Error in writing in STOR\n");
+            printf("%d\n", words);
+            rewind(f);
+            int ch = 0;
+            while (ch < words)
+            {
+                fscanf(f, "%s", buffer);
+                // printf("%s ",buffer);
+                n = write(sockfd, buffer, sizeof(buffer));
+                if (n < 0)
+                    error("Error in writing in STOR\n");
+                // ch = fgetc(f);
+                ch++;
+            }
+            fclose(f);
+            printf("The file was sent successfully. \n");
+            //i=0;
+        }
+        // else if(strncmp(input,"RETR",4) == 0){
+        //         strcpy(buffer,input);
+        //         write(sockfd,buffer,100);
+        //         sscanf(input,"%*s %s",filename);
+        //         printf("the filename is %s",filename);
+        //         read(sockfd,&size,sizeof(int));
+        //         if (size == 530){
+        //             errormsg(size);
+        //         }
+        //         else if (!size){
+        //             error("no such file in remote directory");
+
+        //         }
+        //         f = malloc(size);
+        //         read(sockfd,f,size);
+        //         while(1){
+        //             filehandle  = open(filename ,O_CREAT | O_EXCL | O_WRONLY, 0666);
+        //             if(filehandle == -1){
+        //                 sprintf(filename + strlen(filename),"%d",i);
+        //             }
+        //             else break;
+
+        //         }
+        //         write(filehandle,f,size);
+        //         close(filehandle);
+        //         strcpy(buffer,"cat ");
+        //         strcat(buffer,filename);
+        //         system(buffer);
+        //     }
+        else if (strncmp(input, "RETR", 4) == 0 || strncmp(command, "retr", 4) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in RETR\n");
+            n = read(sockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in reading command\n");
+            printf("Receiving the file from server : \n");
+            FILE *fp;
+            int ch = 0;
+            fp = fopen("RetFROMServer.txt", "a");
+            int words;
+            n = read(sockfd, &words, sizeof(int));
+            if (n < 0)
+                error("Error in reading command\n");
+            while (ch < words)
+            {
+                // printf("%s ",buffer);
+                read(sockfd, buffer, sizeof(buffer));
+                fprintf(fp, "%s ", buffer);
+                ch++;
+            }
+            fclose(fp);
+            printf("The file was received successfully\n");
+        }
+        else if (strncmp(input, "ABOR", 4) == 0 || strncmp(command, "abor", 4) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in ABOR\n");
+            n = read(sockfd, &status, 100);
+            if (n < 0)
+                error("Error in reading command\n");
+            if (status)
+            {
+                printf("Session reset\n");
+            }
+            else
+            {
+                errormsg(status);
+            }
+        }
+
+        else if (strncmp(input, "QUIT", 4) == 0 || strncmp(command, "quit", 4) == 0)
+        {
+            strcpy(buffer, input);
+            n = write(sockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing in QUIT\n");
+            n = read(sockfd, &status, 100);
+            if (n < 0)
+                error("Error in reading command\n");
+            if (status)
+            {
+                printf("Server closed\nQuitting..\n");
+                exit(0);
+            }
+            printf("Server failed to close connection\n");
+        }
+        else
+        {
+            status = 502;
+            errormsg(status);
+        }
+    }
+
+    close(sockfd);
+}
---
+++ server/server.c
@@ -0,0 +1,423 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/stat.h>
+
+#include <sys/sendfile.h>
+
+#include <fcntl.h>
+
+void request(int newsockfd);
+void error(const char *msg)
+{
+    perror(msg);
+    exit(1);
+}
+int sockfd, newsockfd, port_no, i, pid, n, size, status = 0, loggedin = 0;
+
+struct stat obj, st = {0};
+struct sockaddr_in serv_addr, client_addr;
+socklen_t clilen;
+char buffer[100] = {0}, command[4] = {0}, filename[20] = {0}; //changed
+char username[20] = {0};
+char password[20] = {0};
+char firstname[20] = {0};
+char pass_verify[20] = {0};
+char string_0[256] = {0};
+int filehandle;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Usage: ./server <port number>");
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        error("Error Creating Socket");
+    }
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
+    port_no = atoi(argv[1]);
+
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port_no);
+
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+        error("Error in Binding to Socket");
+
+    listen(sockfd, 5);
+    clilen = sizeof(client_addr);
+    while (1)
+    {
+        newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &clilen);
+        if (newsockfd < 0)
+        {
+            error("Error in creating new socket");
+        }
+        pid = fork();
+        if (pid < 0)
+            error("Error in fork");
+        if (pid == 0)
+        {
+            //close(sockfd);
+            while (1)
+            {
+                request(newsockfd);
+            }
+        }
+        else
+        {
+            close(newsockfd);
+        }
+    }
+    close(sockfd);
+}
+
+void request(int newsockfd)
+{
+
+    write(newsockfd, "FTP> ", sizeof("FTP> "));
+    n = read(newsockfd, buffer, 100);
+    if (n < 0)
+        error("Error in reading command\n");
+    sscanf(buffer, "%s", command);
+    printf("Command Recieved [%s] : %s \n", username, command);
+    if (strncmp(command, "CRET", 4) == 0 || strncmp(command, "cret", 4) == 0)
+    {
+        FILE *filePointer;
+        filePointer = fopen("login_info.txt", "a");
+        fputs(buffer + 5, filePointer);
+        fputs("\n", filePointer);
+        fclose(filePointer);
+        status = 1;
+        write(newsockfd, &status, sizeof(int));
+        if (n < 0)
+            error("Error in writing to client\n");
+    }
+
+    else if (strncmp(command, "USER", 4) == 0 || strncmp(command, "user", 4) == 0)
+    {
+        status = 0;
+        FILE *filePointer;
+        filePointer = fopen("login_info.txt", "r+");
+        sscanf(buffer + 5, "%s", username);
+        while ((fgets(string_0, 256, filePointer)) != NULL)
+        {
+
+            sscanf(string_0, "%s", firstname);
+
+            if (strcmp(firstname, username) == 0)
+            {
+                status = 331;
+                printf("Username Matched, Enter Password\n");
+                break;
+            }
+            else
+            {
+                status = 332;
+            }
+        }
+        fclose(filePointer);
+        n = write(newsockfd, &status, sizeof(int));
+        if (n < 0)
+            error("Error in writing to client\n");
+    }
+    else if (strncmp(command, "PASS", 4) == 0 || strncmp(command, "pass", 4) == 0)
+    {
+        FILE *filePointer;
+        filePointer = fopen("login_info.txt", "r+");
+        sscanf(buffer + 5, "%s", password);
+
+        while (fgets(string_0, 256, filePointer) != NULL)
+        {
+            sscanf(string_0, "%s %s", firstname, pass_verify);
+
+            if (strcmp(firstname, username) == 0 && strcmp(pass_verify, password) == 0)
+            {
+                status = 331;
+                loggedin = 1;
+                printf("Password Matched, User Logged In\n");
+                break;
+            }
+        }
+        fclose(filePointer);
+        n = write(newsockfd, &status, sizeof(int));
+        if (n < 0)
+        {
+            error("Error in writing password\n");
+        }
+    }
+    else if (strncmp(command, "PWD", 3) == 0 || strncmp(command, "pwd", 3) == 0)
+    {
+        if (!loggedin)
+        {
+            status = 530;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+        else
+        {
+            system("pwd >> pwd_file.txt");
+            int j = 0;
+            FILE *f = fopen("pwd_file.txt", "r");
+            while (!feof(f))
+            {
+                buffer[j++] = fgetc(f);
+            }
+            buffer[j - 1] = '\0';
+
+            fclose(f);
+            n = write(newsockfd, buffer, 100);
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+    }
+    else if (strncmp(command, "CWD", 3) == 0 || strncmp(command, "cwd", 3) == 0)
+    {
+        if (!loggedin)
+        {
+            status = 530;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+        else
+        {
+            printf("executing command cd \n ");
+            char path[50];
+            sscanf(buffer, " %*s %s", path);
+            if (chdir(path) == 0)
+            {
+                status = 1;
+                printf("Directory changed\n");
+            }
+            else
+                status = 0;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+            printf("\n%d\n", status);
+            printf("\ncd executed");
+            system("pwd >> temp.txt");
+        }
+    }
+    else if (strncmp(command, "RMD", 3) == 0 || strncmp(command, "rmd", 3) == 0)
+    {
+        if (!loggedin)
+        {
+            status = 530;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+        else
+        {
+            printf("executing command rmd \n ");
+            char path[50];
+            sscanf(buffer, " %*s %s", path);
+            if (rmdir(path) == 0)
+            {
+                status = 1;
+                printf("Directory Deleted\n");
+            }
+            else
+            {
+                status = 0;
+            }
+
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+            printf("\nrmd executed");
+        }
+    }
+    else if (strncmp(command, "MKD", 3) == 0 || strncmp(command, "mkd", 3) == 0)
+    {
+        if (!loggedin)
+        {
+            status = 530;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+        else
+        {
+            printf("Executing command mkd \n");
+            char path[50];
+            sscanf(buffer, " %*s %s", path);
+            if (mkdir(path, 0777) == 0)
+            {
+                status = 1;
+                printf("Directory created\n");
+            }
+            else
+            {
+                status = 0;
+            }
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+            printf("\nmkd executed");
+        }
+    }
+    else if (!strcmp(command, "LIST"))
+    {
+        if (!loggedin)
+        {
+            status = 530;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+        else
+        {
+            system("ls >list_file.txt");
+            i = 0;
+            stat("list_file.txt", &obj);
+            size = obj.st_size;
+            n = write(newsockfd, &size, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+            filehandle = open("list_file.txt", O_RDONLY);
+            sendfile(newsockfd, filehandle, NULL, size);
+        }
+    }
+    else if (strncmp(command, "STOR", 4) == 0 || strncmp(command, "stor", 4) == 0)
+    {
+        if (!loggedin)
+        {
+            status = 530;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+        else
+        {
+            // status = 200;
+            printf("Receiving the file from client : \n");
+            FILE *fp;
+            int ch = 0;
+            fp = fopen("received.txt", "a");
+            int words;
+
+            n = read(newsockfd, &words, sizeof(int));
+            if (n < 0)
+                error("Error in reading command\n");
+            printf("%d\n", words);
+            while (ch < words)
+            {
+                // printf("%s ",buffer);
+                n = read(newsockfd, buffer, sizeof(buffer));
+                if (n < 0)
+                    error("Error in reading command\n");
+                fprintf(fp, "%s ", buffer);
+                ch++;
+            }
+            fputs("\n", fp);
+
+            fclose(fp);
+            printf("The file was received successfully\n");
+        }
+    }
+
+    // else if(strcmp(command,"RETR") == 0){
+    //     if(!loggedin){
+    //         status = 530;
+    //         write(newsockfd,&status,sizeof(int));
+    //     }else{
+    //         sscanf(buffer,"%*s %s",filename);
+    //         printf("filename: %s",filename);
+    //         stat(filename,&obj);
+    //         filehandle = open(filename, O_RDONLY);
+    //         size = obj.st_size;
+    //         if(filehandle == -1)
+    //             size = 0;
+    //         write(newsockfd,&size,sizeof(int));
+    //         if(size)
+    //             sendfile(newsockfd,filehandle,NULL,size);
+
+    //     }
+    // }
+    else if (strncmp(command, "RETR", 4) == 0 || strncmp(command, "retr", 4) == 0)
+    {
+        if (!loggedin)
+        {
+            status = 530;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+        }
+        else
+        {
+            status = 200;
+            n = write(newsockfd, &status, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+            FILE *f;
+            int words = 0;
+            char c;
+            char file[50];
+            sscanf(buffer, " %*s %s", file);
+            f = fopen(file, "r");
+            while ((c = getc(f)) != EOF)
+            {
+                fscanf(f, " %s", buffer);
+                if (isspace(c) || c == '\t')
+                {
+                    words++;
+                }
+            }
+            n = write(sockfd, &words, sizeof(int));
+            if (n < 0)
+                error("Error in writing to client\n");
+            rewind(f);
+            int ch = 0;
+            while (ch < words)
+            {
+                fscanf(f, "%s", buffer);
+                // printf("%s ",buffer);
+                ch++;
+                n = write(newsockfd, buffer, sizeof(buffer));
+                if (n < 0)
+                    error("Error in writing to client\n");
+            }
+            fclose(f);
+            printf("The file was sent successfully. \n");
+        }
+    }
+    else if (strncmp(command, "ABOR", 4) == 0 || strncmp(command, "abor", 4) == 0)
+    {
+        loggedin = 0;
+        i = 0;
+        n = write(newsockfd, &i, sizeof(int));
+        if (n < 0)
+            error("Error in writing to client\n");
+    }
+    else if (strncmp(command, "QUIT", 4) == 0 || strncmp(command, "quit", 4) == 0)
+    {
+        printf("FTP server quitting..\n");
+        i = 1;
+        n = write(newsockfd, &i, sizeof(int));
+        if (n < 0)
+            error("Error in writing to client\n");
+        exit(0);
+    }
+    else
+    {
+        char *msg = "no such command ";
+        n = write(newsockfd, &msg, sizeof(msg));
+        if (n < 0)
+            error("Error in writing to client\n");
+    }
+}
